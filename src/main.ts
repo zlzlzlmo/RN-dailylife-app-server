cccd6dc04aca60a67cd4370a203213244e2a4543
신승훈 <zlzlzlmo@daum.net>
2022-06-19 10:11:55 +0900

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
